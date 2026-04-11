aef437800d4fe4f212854a6e210034797a55401a
Sujal Rajapure <sujalrajapure343@gmail.com>
2026-04-11 15:38:16 +0530

    payment changed code

--- rydex/src/app/api/payment/create/route.ts
+++ rydex/src/app/api/payment/create/route.ts
@@ -1,33 +1,43 @@
-import { NextResponse } from "next/server"
-import razorpay from "@/lib/razorpay"
-import connectDb from "@/lib/db"
-import Booking from "@/models/booking.model"
+import { NextRequest, NextResponse } from "next/server";
+import razorpay from "@/lib/razorpay";
+import connectDb from "@/lib/db";
+import Booking from "@/models/booking.model";
 
+export async function POST(req: NextRequest) {
+  try {
+    await connectDb();
 
+    const { bookingId } = await req.json();
 
-export async function POST(req: Request) {
+    const booking = await Booking.findById(bookingId);
 
-  await connectDb()
+    if (!booking) {
+      return NextResponse.json(
+        { error: "Booking not found" },
+        { status: 404 }
+      );
+    }
 
-  const { bookingId } = await req.json()
+    const order = await razorpay.orders.create({
+      amount: booking.fare * 100,
+      currency: "INR",
+      receipt: booking._id.toString(),
+    });
 
-  const booking = await Booking.findById(bookingId)
+    booking.status = "awaiting_payment";
+    await booking.save();
 
-  if (!booking) {
-    return NextResponse.json({ error: "Booking not found" })
+    return NextResponse.json({
+      orderId: order.id,
+      amount: order.amount,
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      { error: "Payment order creation failed" },
+      { status: 500 }
+    );
   }
-
-  const order = await razorpay.orders.create({
-    amount: booking.fare * 100,
-    currency: "INR",
-    receipt: booking._id.toString(),
-  })
-
-  booking.status = "awaiting_payment"
-  await booking.save()
-
-  return NextResponse.json({
-    orderId: order.id,
-    amount: order.amount
-  })
 }
